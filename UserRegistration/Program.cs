61cfff019919b2935c9c265f2055a012b304c12b
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-19 22:47:22 +0530

    Validating the Given First Name As Per Regular Expression Rule

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n***** Welcome To User Registration *****");
+
+            Console.Write("\n\nEnter First Name :  ");
+            string FirstName = Console.ReadLine();
+            string RegexRules = "^[A-Z]{1}[a-z]{2,}$";
+            Validation.validateString(FirstName, RegexRules);
+            Console.ReadLine();
+
+        }
+    }
+    
+}
---
+++ UserRegistration/Validation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Validation
+    {
+        public static void  validateString(string sample,string RegexRule)
+        {
+            if( Regex.IsMatch(sample, RegexRule))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\n\n"+sample+ " is Valid Input !");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("\n\n" + sample +" is Invalid Input !");
+                Console.ResetColor();
+
+            }
+        }
+    }
+}
