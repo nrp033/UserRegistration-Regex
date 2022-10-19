c86e0f29f6ccf53d6492f16eecbda4a111d1e665
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-19 23:39:48 +0530

    Validating the Given Email Using Regular Expression

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace UserRegistration
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace UserRegistration
 {
     internal class Program
     {
@@ -18,7 +21,7 @@
                     break;
 
                 case 1:
-
+                  //First name starts with Cap and has minimum 3 characters
                     Console.Write("\n\nEnter First Name :  ");
                     UserInput = Console.ReadLine();
                     RegexRules = "^[A-Z]{1}[a-z]{2,}$";
@@ -27,9 +30,20 @@
                     break;
 
                 case 2:
+                  //Name - Last name starts with Cap and has minimum 3 characters
                     Console.Write("\n\nEnter Last Name :  ");
                     UserInput = Console.ReadLine();
                     RegexRules = "^[A-Z]{1}[a-z]{2,}$";
+                    Validation.validateString(UserInput, RegexRules);
+                    goto Home;
+                    break;
+                    //abc.xyz @bl.co.in
+
+                case 3:
+                   // E.g.abc.xyz @bl.co.in
+                    Console.Write("\n\nEnter Email :  ");
+                    UserInput = Console.ReadLine();
+                    RegexRules = "^[a-z]+([._][A-Za-z0-9]+)*[@][A-Za-z]+[.][A-Za-z]{2,3}([.][A-Za-z]{2})?$";
                     Validation.validateString(UserInput, RegexRules);
                     goto Home;
                     break;
@@ -50,6 +64,7 @@
         {
             Console.WriteLine("\n\n1) Validate First Name ");
             Console.WriteLine("2) Validate Last Name ");
+            Console.WriteLine("3) Validate Email ID");
 
 
             Console.WriteLine("\n\n\nPress 0 To Exit !");
